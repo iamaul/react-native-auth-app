07c15dac75cead6a87fd4015a2e4aab3c0b362aa
Agung Maulana <iamaul@192.168.0.102>
2024-01-01 19:16:14 +0700

    chore: babel & metro config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@assets': './src/assets',
+          '@config': './src/config',
+          '@contexts': './src/contexts',
+          '@layouts': './src/layouts',
+          '@components': './src/components',
+          '@common': './src/common',
+          '@screens': './src/screens',
+          '@navigations': './src/navigations',
+        },
+      },
+    ],
+  ],
 };
